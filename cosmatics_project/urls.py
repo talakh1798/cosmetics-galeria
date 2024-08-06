1c3cfbe4d60c3884be95cc8abbf3ab57263c447f
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-08-06 16:50:09 +0300

    media makeup

--- cosmatics_project/urls.py
+++ cosmatics_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+   
     path('admin/', admin.site.urls),
-    path('', include('cosmatics_app.urls')),  # Include cosmatics app's URL patterns  
+    path('', include('cosmatics_app.urls')),  # Include cosmatics app's URL patterns 
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
